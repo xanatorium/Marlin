3d4d2ab0497dc024a6f4aa7c401ba84a0aec63b7
xanatorium <kschaetzmueller@gmail.com>
2021-07-21 11:18:52 +0200

    PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -522,9 +522,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    #define DEFAULT_Kp 17.78
+    #define DEFAULT_Ki 1.20
+    #define DEFAULT_Kd 65.95
   #endif
 #endif // PIDTEMP
 
